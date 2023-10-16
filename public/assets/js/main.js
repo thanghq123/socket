67201bc5f6fcb9af30f1edcbb84b95bf4154206d
dinhtv7 <nguyenduyquangvinh2906@gmail.com>
2023-10-16 15:11:18 +0700

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -150,17 +150,11 @@ ${user.total_point}
 <td class="px-6 py-4 w-[50%]">
     <div class="w-[100%] bg-[#f0f0f0] rounded-lg h-12 inline-flex">
         <div class="bg-green-400 inline-flex ml-1.5 mt-1 h-10 rounded-l-lg" style="width: ${
-                (Number(user.total_point) / (Number(user.total_point) + Number(user.errors))) * 100
-            }%">
-            <svg class="mt-2.5 ml-2 w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
-                <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
-            </svg>
+                (Number(user.total_point))
+            }px">
             <p class="pl-0.5 pt-2 font-bold text-white">${user.total_point}</p>
 <!--            <p class="pl-0.5 pt-2 font-bold text-white">5</p>-->
         </div>
-        <div class="bg-red-400 mt-1 h-10 rounded-r-lg" style="width: ${
-                (Number(user.errors) / (Number(user.total_point) + Number(user.errors))) * 100
-            }%"></div>
     </div>
 </td>
 </tr>
